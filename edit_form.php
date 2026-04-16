d9c43c88401e58774454d1b8b33b46eb5f28a3c4
Luiggi Sansonetti <1565841+luiggisanso@users.noreply.github.com>
2026-04-16 11:49:57 +0200

    Update edit_form.php

--- edit_form.php
+++ edit_form.php
@@ -22,8 +22,8 @@ require_once("$CFG->libdir/formslib.php");
  * My Progress block configuration form definition
  *
  * @package    block_myprogress
- * @copyright  2023 e-Learning – Conseils & Solutions <http://www.luiggisansonetti.fr/conseils>
- * @author     Willian Mano <willianmanoaraujo@gmail.com>
+ * @copyright  2026 E-learning Touch' <contact@elearningtouch.com> (Maintainer)
+ * @author     2023 Willian Mano <willianmanoaraujo@gmail.com>
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 class block_myprogress_edit_form extends block_edit_form {
